92ef51305a0754aa7d8fa05327db5fff4d213671
mike-neck <jkrt3333@gmail.com>
2024-01-16 17:03:58 +0900

    オプションを指定

--- features/step_definitions/breave_himmel.rb
+++ features/step_definitions/breave_himmel.rb
@@ -1,7 +1,12 @@
 require 'selenium-webdriver'
 
 Given('he was on a hill near a city destroyed by the attack') do
-  @driver = Selenium::WebDriver.for :chrome
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+
+  @driver = Selenium::WebDriver.for :chrome, options: options
   @driver.get 'https://www.google.jp'
 end
 
